f3d84f171eb2eb09d454d3445b2225758f1db0c9
Daniel Allison <github@danielallison.dev>
2024-04-12 21:52:27 -0400

    feat(DataDisksRole): Add data disks role which creates xfs and ext4 partitions, then mounts them

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,9 +30,9 @@ variable "project_metadata" {
     Environment    = string
   })
   default = {
-    Assignment     = "CCGC 5502 Automation Assignment"
+    Assignment     = "CCGC 5502 Automation Project"
     Name           = "Daniel.Allison"
     ExpirationDate = "2024-12-31"
-    Environment    = "Learning"
+    Environment    = "Project"
   }
 }
